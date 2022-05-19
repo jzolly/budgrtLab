787e0f888939b858fd97e3efe6982253eaa59315
Jessie Zollinger <jzolly@hotmail.com>
2022-05-18 22:47:03 -0400

    finished

--- Controllers/server.js
+++ Controllers/server.js
@@ -1,6 +1,6 @@
 // required dependencies
 const express = require('express');
-const budgets = require('../models/budget')
+const budget = require('../models/budget')
 
 // initialize the epxress application
 const app = express();
@@ -10,27 +10,31 @@ const port = 3000;
 
 // mount our middleware
 // app.use(express.static('../public'))
+app.use(express.urlencoded({ extended: false}))
 
 // Index
 app.get('/budgets', (req, res) => {
-    res.render('index.ejs', {'budget': budgets})
+    res.render('index.ejs', {'budget': budget})
 });
 
 // New.
 app.get('/budgets/new', (req, res) => {
-    res.render('new.ejs')
+    res.render('new.ejs');
 })
 // D.
 // U.
 // Create.
-app.post('/budgets', (req, res) => {
+app.post("/budgets", (req, res) => {
+    budget.push(req.body)
+    console.log(budget)
+    res.redirect('/budgets')
+  })
 
-})
 // E.
 // Show.
 app.get('/budgets/:indexOfBudgetArray', (req, res) => {
     res.render('show.ejs', {
-    'budget': budgets[req.params.indexOfBudgetArray]   
+    'budget': budget[req.params.indexOfBudgetArray]   
     });
 });
 
