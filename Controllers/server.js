63f859918c40b854f0e60b060e0fa8768d831d2d
Jessie Zollinger <jzolly@hotmail.com>
2022-05-18 21:44:39 -0400

    show page

--- Controllers/server.js
+++ Controllers/server.js
@@ -1,6 +1,6 @@
 // required dependencies
 const express = require('express');
-const budget = require('../models/budget')
+const budgets = require('../models/budget')
 
 // initialize the epxress application
 const app = express();
@@ -9,17 +9,16 @@ const app = express();
 const port = 3000;
 
 // mount our middleware
-app.use(express.static('../public'))
+// app.use(express.static('../public'))
 
 // Index
 app.get('/budgets', (req, res) => {
-    res.render('index.ejs', {'budget': budget})
-    console.log(budget)
+    res.render('index.ejs', {'budget': budgets})
 });
 
 // New.
 app.get('/budgets/new', (req, res) => {
-    res.send('new.ejs')
+    res.render('new.ejs')
 })
 // D.
 // U.
@@ -29,9 +28,12 @@ app.post('/budgets', (req, res) => {
 })
 // E.
 // Show.
-app.get('/budgets/:index', (req, res) => {
-    res.send('show.ejs')
-})
+app.get('/budgets/:indexOfBudgetArray', (req, res) => {
+    res.render('show.ejs', {
+    'budget': budgets[req.params.indexOfBudgetArray]   
+    });
+});
+
 // this tells node to listen for requests is running on: localhost:3000
 app.listen(port, () => {
     console.log('listening on port', port)
