a5998a995175848fad40cac62abf2c6f179c9eaf
Jessie Zollinger <jzolly@hotmail.com>
2022-05-18 20:53:38 -0400

    used http instead of

--- Controllers/server.js
+++ Controllers/server.js
@@ -4,12 +4,12 @@ const budget = require('../models/budget')
 // initialize the epxress application
 const app = express();
 // configure application settings
-
+const port = 3000;
 // mount our middleware
-
+app.use(express.static('../public'))
 // Index
 app.get('/budgets', (req, res) => {
-    res.send('budgetIndex.ejs')
+    res.render('index.ejs', {'budget': budget})
 });
 // New.
 app.get('/budgets/new', (req, res) => {
@@ -27,6 +27,6 @@ app.get('/budgets/:index', (req, res) => {
     res.send('show.ejs')
 })
 // this tells node to listen for requests is running on: localhost:3000
-app.listen(3000, () => {
-    console.log('')
+app.listen(port, () => {
+    console.log('listening on port', port)
 });
